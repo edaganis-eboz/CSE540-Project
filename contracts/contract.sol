b05697c7213c20430bace11aaf1b709dc7bf9cb1
Erin Ozcan <ebozcan@asu.edu>
2026-04-08 14:29:44 -0700

    Fleshed out create functions, working on sigs

--- contracts/contract.sol
+++ contracts/contract.sol
@@ -3,6 +3,9 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 import "hardhat/console.sol";
+// please use camelCase
+// TODO:
+// add require checks to create functions
 
 contract SupplyChainProvenance {
     // The data types we'll probably need
@@ -15,9 +18,17 @@ contract SupplyChainProvenance {
         Regulator,
         Consumer
     }
-    struct itemStatus {
+    enum itemStatus {
         // dont know if we should use a enum here
-        string status;
+        created,
+        atSupplier,
+        atManufacturer,
+        atDistributor,
+        atRetailer,
+        atRegulator,
+        atConsumer,
+        retired,
+        onRoute
     }
     struct actor {
         actorRole role;
@@ -42,7 +53,7 @@ contract SupplyChainProvenance {
     mapping(address => actor) public actorList;
     uint16 nextItemIndex = 1; //THE ITEM ID WILL BE THE ITEMS INDEX ON THIS LIST
     address public owner; 
-    //Okay so for now we're gonna have a global owner, this is the person who can register actors and probably other things
+    // Okay so for now we're gonna have a global owner, this is the person who can register actors and probably other things
     
     // For now just gonna write some function signatures
     modifier onlyOwner() {
@@ -52,27 +63,62 @@ contract SupplyChainProvenance {
     constructor() { // This function requires alot of gas for some reason
         owner = msg.sender;
     }
-    function registerActor(address actorAddress, actorRole role) external onlyOwner {
+
+    // All the events we'll need
+    event actorRegistered(address indexed actorAddr, actorRole role);
+    event itemCreated(uint256 indexed itemId, string itemName, address indexed creatorAddr);
+    // When an item gets signed, assume its in a new status
+    event itemSigned(
+        uint256 indexed itemId,
+        address indexed signerAddr,
+        actorRole role,
+        itemStatus updatedStatus,
+        string note
+    );
+
+    function registerActor(address actorAddr, actorRole role) external onlyOwner {
         // Should add an actor to a list
+        actorList[actorAddr] = actor({
+            role: role,
+            actorAddress: actorAddr,
+            exists: true
+        });
+        // emit actorRegistered event
+        emit actorRegistered(actorAddr, role);
     }
     function createItem(string calldata itemName, string calldata metadata) external returns (uint256) {
         // Create an item object
-        //add it to the itemList
+        // add it to the itemList
         // increment nextItemIndex
         uint256 itemId = nextItemIndex;
+        itemList[itemId] = item({
+            itemId: itemId,
+            itemName: itemName,
+            metadata: metadata,
+            creator: msg.sender,
+            status: itemStatus.created,
+            exists: true
+        });
         nextItemIndex++; 
+        // emit itemCreated event
+        emit itemCreated(itemId, itemName, msg.sender);
         return itemId;
     }
-    function actorSign(uint256 itemId, string calldata signatureNote) external  {
-        // This function will be used for an actor to sign their action
-        // May need to be split up into 6 different functions, one for each actor, 
+    function actorReceiveSign(uint256 itemId, string calldata signatureNote) external  {
+        // This function will be used for an actor to sign when they receive an item
+        // Thus the itemstatus should be "onRoute" and then changed to "atWhoever"
+        
+    }
+    function actorSendSign(uint256 itemId, string calldata signatureNote) external  {
+        // This function will be used for an actor to sign when they send out an item
+        // Thus itemstatus should be "atWhoever" and then changed to "onRoute"
     }
     function getItem(uint256 itemId) external view returns (
         uint256 id,
         string memory itemName,
         string memory itemMetadata,
         address itemCreator,
-        itemStatus memory status
+        itemStatus status
     ) // return struct data as a tuple for reasons, trust me bro
     {
         // Find the item on the mapping using itemID as index
